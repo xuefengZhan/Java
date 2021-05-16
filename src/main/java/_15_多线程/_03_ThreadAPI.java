b38c089b2713ef36a7159abdae4807770c4b785b
fantasy <taiohj@163.com>
2021-05-17 00:16:51 +0800

    多线程01

--- src/main/java/_15_多线程/_03_ThreadAPI.java
+++ src/main/java/_15_多线程/_03_ThreadAPI.java
@@ -8,6 +8,13 @@ public class _03_ThreadAPI {
     //todo 1.public static void sleep
     @Test
     public void test01(){
-
+        for (int i = 10; i >= 1 ; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
