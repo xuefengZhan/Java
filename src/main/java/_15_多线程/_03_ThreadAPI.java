ab4821b5b512ab2858af2146f38bd08857dd9002
fantasy <taiohj@163.com>
2021-05-18 00:18:30 +0800

    多线程01

--- src/main/java/_15_多线程/_03_ThreadAPI.java
+++ src/main/java/_15_多线程/_03_ThreadAPI.java
@@ -8,7 +8,7 @@ public class _03_ThreadAPI {
     //todo 1.public static void sleep
     @Test
     public void test01(){
-        for (int i = 10; i >= 1 ; i++) {
+        for (int i = 10; i >= 1 ; i--) {
             System.out.println(i);
             try {
                 Thread.sleep(1000);
@@ -16,5 +16,14 @@ public class _03_ThreadAPI {
                 e.printStackTrace();
             }
         }
+    }
+
+
+    //todo 2.public String getName() 获取Thread的名字
+    //todo 3.public static Thread currentThread() 获取当前正在运行的Thread对象
+    @Test
+    public void test02(){
+        Thread t = Thread.currentThread();
+        System.out.println(t.getName());//main
     }
 }
