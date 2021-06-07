d2b435147cc52a57b22337d88c80061328ea827b
zhanxuefeng <yatolove@163.com>
2021-06-07 12:50:49 +0800

    s

---
+++ src/main/java/_01_/运算符.java
@@ -0,0 +1,22 @@
+package _01_;
+
+import org.junit.Test;
+
+public class 运算符 {
+    @Test
+    public void test01(){
+
+
+        System.out.println(5/2);//2
+        System.out.println(5/-3);//-1
+        System.out.println(5%2);//1
+        System.out.println(5%-3);//2
+
+        System.out.println(-7/2);//-3
+        System.out.println(-7/-3);//2
+        System.out.println(-7%2);//-1
+        System.out.println(-7%-2);//-1
+
+        //取模的符号跟左边对其
+    }
+}
