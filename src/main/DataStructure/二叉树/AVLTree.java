82e8de0bbea240889b47edfe067efa4b937fc9a5
zhanxuefeng <yatolove@163.com>
2021-09-10 18:29:54 +0800

    AVLTree

---
+++ src/main/DataStructure/二叉树/AVLTree.java
@@ -0,0 +1,188 @@
+package 二叉树;
+
+import 二叉树.BinaryTreePrinter.src.com.mj.printer.BinaryTrees;
+
+import java.util.Comparator;
+
+public class AVLTree<E> extends BinarySearchTree<E> {
+
+    public AVLTree() {
+    }
+
+    public AVLTree(Comparator comparator) {
+        super(comparator);
+    }
+
+    private class AVLNode<E> extends Node<E> {
+        private int height = 1; //默认为1，因为平衡因子判断左右子节点height差为2 如果为0有歧义
+
+        public AVLNode() {
+        }
+
+        public AVLNode(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+
+        public int balanceFactor() {
+            int leftHeight = left == null ? 0 : ((AVLNode<E>) left).height;
+            int rightHeight = right == null ? 0 : ((AVLNode<E>) right).height;
+            return Math.abs(leftHeight - rightHeight);
+        }
+
+        public void updateHeight() {
+            height = Math.max(((AVLNode<E>) left).height, ((AVLNode<E>) right).height) + 1;
+        }
+
+        public Node<E> tallerChild() {
+            int leftHeight = left == null ? 0 : ((AVLNode<E>) left).height;
+            int rightHeight = right == null ? 0 : ((AVLNode<E>) right).height;
+
+            return leftHeight > rightHeight ? left : right;
+        }
+
+    }
+
+    private boolean isBalance(Node<E> node) {
+        return ((AVLNode<E>) node).balanceFactor() < 2;
+    }
+
+    private void updateHeight(Node<E> node) {
+        ((AVLNode<E>) node).updateHeight();
+    }
+
+    private Node<E> tallerChild(Node<E> node) {
+        return ((AVLNode<E>) node).tallerChild();
+    }
+
+    @Override
+    protected Node<E> createNode(E element, Node<E> parent) {
+        return new AVLNode<E>(element, parent);
+    }
+
+    private void rotateRight(Node<E> grand) {
+        Node<E> parent = grand.left;
+        Node<E> t3 = parent.right;
+        //Node<E> father = grand.parent;
+
+        parent.right = grand;
+        //grand.parent = parent;
+
+        grand.left = t3;
+        // t3.parent = grand;
+
+        // parent.parent = father;
+        //if(father == null){
+        root = parent;
+        //}else{
+        //    if(grand == father.left){
+        //        father.left = parent;
+        //    }else{
+        //        father.right = parent;
+        //    }
+        //}
+        switchFather(grand, t3, parent);
+    }
+
+    private void rotateLeft(Node<E> grand) {
+        Node<E> parent = grand.right;
+        Node<E> t2 = parent.left;
+        //Node<E> father = grand.parent;
+
+        parent.left = grand;
+        //grand.parent = parent;
+
+        grand.right = t2;
+        //t2.parent = grand;
+
+        //parent.parent = father;
+        //if(father == null){
+        //    root = parent;
+        //}else{
+        //   if(grand == father.left){
+        //        father.left = parent;
+        //   }else{
+        //        father.right = parent;
+        //    }
+        // }
+        switchFather(grand, t2, parent);
+    }
+
+    private void switchFather(Node<E> grand, Node<E> t, Node<E> parent) {
+        Node<E> father = grand.parent;
+        grand.parent = parent;
+        t.parent = grand;
+        parent.parent = father;
+        if (father == null) {
+            root = parent;
+        } else {
+            if (grand == father.left) {
+                father.left = parent;
+            } else {
+                father.right = parent;
+            }
+        }
+
+    }
+
+    protected void rebalance(Node<E> grand) {
+        Node<E> parent = ((AVLNode<E>) grand).tallerChild();
+        Node<E> node = ((AVLNode<E>) parent).tallerChild();
+
+        Node<E> farther = grand.parent;
+
+        if (parent == grand.left) {
+            if (node == parent.left) {//LL
+                rotateRight(grand);
+            } else {//LR
+
+            }
+
+        } else {
+            if (node == parent.left) {//RL
+
+            } else {//RR
+
+            }
+        }
+    }
+
+
+    @Override
+    protected void afterAdd(Node<E> node) {
+        Node<E> parent = node.parent;
+        while (parent != null) {
+            if (isBalance(parent)) {
+                updateHeight(parent);
+                parent = parent.parent;
+            } else {
+                rebalance(parent);
+                break;
+            }
+        }
+    }
+}
+
+class Test2{
+    public static void main(String[] args) {
+        AVLTree<Integer> avlt = new AVLTree<>();
+        //avlt.add()
+        avlt.add(35);
+        avlt.add(37);
+        avlt.add(34);
+        avlt.add(56);
+        avlt.add(25);
+        avlt.add(62);
+        avlt.add(57);
+        avlt.add(9);
+        avlt.add(74);
+        avlt.add(32);
+        avlt.add(94);
+        avlt.add(80);
+        avlt.add(75);
+        avlt.add(100);
+        avlt.add(16);
+        avlt.add(82);
+        BinaryTrees.println(avlt);
+    }
+}
---
+++ src/main/DataStructure/二叉树/BinarySearchTree.java
@@ -0,0 +1,149 @@
+package 二叉树;
+
+import 二叉树.BinaryTreePrinter.src.com.mj.printer.BinaryTreeInfo;
+import 二叉树.BinaryTreePrinter.src.com.mj.printer.BinaryTrees;
+
+import java.util.Comparator;
+
+public class BinarySearchTree<E> extends BinaryTree<E> {
+    protected Comparator<E> comparator;
+
+    public BinarySearchTree(){}
+    public BinarySearchTree(Comparator<E> comparator){
+        this.comparator = comparator;
+    }
+
+    protected int compare(E e1, E e2) {
+        if (comparator == null) {
+            return ((Comparable<E>) e1).compareTo(e2);
+        } else {
+            return comparator.compare(e1, e2);
+        }
+    }
+
+
+    protected Node<E> createNode(E element,Node<E> parent){ return new Node<E>(element,parent);}
+    protected void afterAdd(Node<E> node){}
+
+    public void add(E element) {
+        elementNotNullCheck(element);
+        if (root == null) {
+            root = new Node<E>(element, null);
+            return;
+        }
+
+        Node<E> cur = root;
+        Node<E> parent = null;
+        int cmp = 0;
+        while (cur != null) {
+            cmp = compare(element, cur.element);
+            parent = cur;
+            if (cmp > 0) {
+                cur = cur.right;
+            } else if (cmp < 0) {
+                cur = cur.left;
+            } else {
+                cur.element = element;
+                return;
+            }
+        }
+        //cur == null
+        Node<E> newNode = createNode(element, parent);
+        if (cmp > 0) {
+            parent.right = newNode;
+        } else {
+            parent.left =  newNode;
+        }
+
+        afterAdd(newNode);
+        size++;
+    }
+
+
+    //todo 删除
+    // todo delete  1.根据值找到节点
+    private Node<E> node(E element){
+        elementNotNullCheck(element);
+        Node<E> cur = root;
+        while(cur != null){
+            int cmp = compare(element,cur.element);
+            if(cmp > 0){
+                cur = cur.right;
+            }else if(cmp < 0){
+                cur = cur.left;
+            }else{
+                return cur;
+            }
+        }
+        return null;
+    }
+
+
+    public void remove(E element) {
+        Node<E> node = node(element);
+        if(node == null) return;
+        Node<E> remove;
+        if(node.hasTwoChild()){
+            remove = processor(node);
+            node.element = remove.element;
+        }else{
+            remove = node;
+        }
+
+        //remove 度为1 or 0
+        Node<E> child = remove.left == null? remove.right : remove.left;
+
+        if(child != null){
+            //remove 度为1
+            child.parent = remove.parent;
+        }
+        if(remove.parent == null){
+            root = child;
+        }else{
+            if(remove == remove.parent.left){
+                remove.parent.left = child;
+            }else{
+                remove.parent.right = child;
+            }
+        }
+        size--;
+    }
+
+    public boolean contains(E element){
+        return node(element) != null;
+    }
+
+
+}
+
+class Test {
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        bst.add(8);
+        bst.add(4);
+        bst.add(2);
+        bst.add(1);
+        bst.add(3);
+        bst.add(6);
+        bst.add(5);
+        bst.add(7);
+        bst.add(13);
+        bst.add(10);
+        bst.add(9);
+        bst.add(12);
+        bst.add(11);
+        bst.add(15);
+        bst.add(14);
+
+        BinaryTrees.println(bst);
+        System.out.println(bst.height());
+
+        bst.remove(11);
+        BinaryTrees.println(bst);
+
+        System.out.println(bst.height());
+        System.out.println(bst.isComplete());
+
+
+    }
+}
